aa343267807be9e682d11cf13e1205099849528d
Ali Khosro <102772697+alan-khosro@users.noreply.github.com>
2024-11-30 08:00:21 -0800

    Update examples.ts

--- test/data-samples/examples.ts
+++ test/data-samples/examples.ts
@@ -20,3 +20,48 @@ digraph {
 
 }
 `;
+
+export const reserseArrow = `
+digraph {
+	
+	graph [compound=true, concentrate=true, rankdir=LR, color="#8888ff", fontcolor=blue] 
+	    
+	node[color="#aaaaff", shape=box fontsize=14]     
+	
+	edge[color=blue, labelfloat=true, fontsize=12, fontcolor=brown weight=1]
+
+
+	subgraph message {
+		node[margin=0, shape=cds]
+		
+		Data
+		Question
+		SQL
+	}
+
+
+	User -> Question -> 
+	YourSolution -> TranslateToSQL
+	-> SQL -> DB
+	
+	User -> VisualizeAndInsight -> Data -> DB [dir=back]
+	
+	TranslateToSQL -> MetaData[dir=back]
+	
+	subgraph cluster_LLM {
+		label="LLM"
+		TranslateToSQL
+		VisualizeAndInsight
+	}
+	
+	subgraph cluster_data {
+		label=DataWarehouse
+		
+		DB
+		MetaData
+	} 
+	
+	YourSolution[shape=box3d style=filled fillcolor=lightblue]
+	
+}
+`
